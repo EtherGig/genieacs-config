3735d30af3915833eb0f93403034952f9a7400c4
ethergig <ethergig07@gmail.com>
2025-07-06 19:38:24 +0700

    add checking for PreSharedKey parameter

--- virtual-params/SSID1-Password.js
+++ virtual-params/SSID1-Password.js
@@ -3,8 +3,17 @@ const instanceIndex = '1';
 
 if (args[1].value) {
   m = args[1].value[0];
+
+  // Declare PreSharedKey first
   declare(`InternetGatewayDevice.LANDevice.1.WLANConfiguration.${instanceIndex}.PreSharedKey.1.KeyPassphrase`, null, {value: m});
-  declare(`InternetGatewayDevice.LANDevice.1.WLANConfiguration.${instanceIndex}.KeyPassphrase`, null, {value: m});
+
+  // Try to read back the declared value
+  let pskCheck = declare(`InternetGatewayDevice.LANDevice.1.WLANConfiguration.${instanceIndex}.PreSharedKey.1.KeyPassphrase`, {value: Date.now()});
+  
+  // Only declare the second one if the first did not succeed
+  if (!pskCheck.size || !pskCheck.value) {
+    declare(`InternetGatewayDevice.LANDevice.1.WLANConfiguration.${instanceIndex}.KeyPassphrase`, null, {value: m});
+  }
 }
 else {
   let psk = declare(`InternetGatewayDevice.LANDevice.1.WLANConfiguration.${instanceIndex}.PreSharedKey.1.KeyPassphrase`, {value: Date.now()});
